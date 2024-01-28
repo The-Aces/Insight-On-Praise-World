322d0fa5b4e7863724c7dc17a77555002096d3ff
Praise Ebuka <praise.njoga@gmail.com>
2024-01-28 20:09:38 +0100

    yeah, sure but not now....still cooking

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,12 +33,18 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'phone' => ['required', 'integer', 'phone', 'max:255'],
+            'country' => ['required', 'string', 'country', 'max:255'],
+            'state' => ['required', 'string', 'state', 'max:255'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'phone' => $request->phone,
+            'country' => $request->country,
+            'state' => $request->state,
             'password' => Hash::make($request->password),
         ]);
 
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->integer('phone');
+            $table->string('country');
+            $table->string('state');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
