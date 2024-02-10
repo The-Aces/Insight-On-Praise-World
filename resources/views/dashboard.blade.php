ca0f6eaa96277c7bad28950115f151cbca90a61c
Praise Ebuka <praise.njoga@gmail.com>
2024-02-10 19:10:35 +0100

    I'm out of ideas fr!!!

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,6 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("Hello, " . Auth::user()->name) }}
-                    <p> Click <a href="#">here</a> to go to the Socials</p>
                 </div>
             </div>
         </div> <br>
